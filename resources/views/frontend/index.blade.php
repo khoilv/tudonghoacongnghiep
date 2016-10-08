cfa22b64f5172e648d1725ae40e8f752a4bfbaa5
khoilv <laivankhoi318@gmail.com>
2016-10-09 04:58:55 +0700

    Project skeleton

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,15 +6,15 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="{{ asset('styles/frontend/vendor.css') }}">
-    <link rel="stylesheet" href="{{ asset('styles/frontend/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/vendor.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/app.css') }}">
 </head>
 <body><!--[if lt IE 10]>
 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
     your browser</a> to improve your experience.</p>
 <![endif]-->
 <div ui-view=""></div>
-<script src="{{ asset('scripts/frontend/vendor.js') }}"></script>
-<script src="{{ asset('scripts/frontend/app.js') }}"></script>
+<script src="{{ asset('frontend/vendor.js') }}"></script>
+<script src="{{ asset('frontend/app.js') }}"></script>
 </body>
 </html>
