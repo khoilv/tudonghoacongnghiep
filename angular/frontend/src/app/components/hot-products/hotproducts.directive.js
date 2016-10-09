46366f90d379507708bc140c7349996837d60e74
khoilv <laivankhoi318@gmail.com>
2016-10-09 23:32:40 +0700

    Home page: Coding hot products slide

--- angular/frontend/src/app/components/hot-products/hotproducts.directive.js
+++ angular/frontend/src/app/components/hot-products/hotproducts.directive.js
@@ -16,13 +16,42 @@
             },
             controller: HotProductsController,
             controllerAs: 'vm',
-            bindToController: true
+            bindToController: false
         };
 
-        return directive;
+        HotProductsController.$inject = ['$scope'];
 
         /** @ngInject */
-        function HotProductsController() {}
+        function HotProductsController($scope) {
+            $scope.myInterval = 5000;
+            $scope.active = 0;
+            $scope.noWrapSlides = false;
+            $scope.slides = [];
+
+            init();
+
+            function init() {
+                var slide = [];
+                var i;
+                for(i = 0; i < 4; i++) {
+                    slide.push({
+                        discount_rate: '20%',
+                        product_image: '../../../assets/images/frontend/img_05.jpg',
+                        product_name: 'Bộ chuyển đổi USB 2.0 to RS232',
+                        product_old_price: '200.000',
+                        product_new_price: '100.000'
+                    });
+                }
+                for (i = 0; i < 2; i++) {
+                    $scope.slides.push({
+                        id: i,
+                        products: slide
+                    });
+                }
+            }
+        }
+
+        return directive;
     }
 
 })();
