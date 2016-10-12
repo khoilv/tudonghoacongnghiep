a46a7ee52bb089c8d05b4aa2245be9505720d216
khoilv <laivankhoi318@gmail.com>
2016-10-12 16:58:33 +0700

    Home page: Coding Online Supports

--- angular/frontend/src/app/index.config.js
+++ angular/frontend/src/app/index.config.js
@@ -6,9 +6,11 @@
         .config(config);
 
     /** @ngInject */
-    function config($logProvider, toastr) {
+    function config($logProvider, $compileProvider, toastr) {
         // Enable log
         $logProvider.debugEnabled(true);
+
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|skype):/);
 
         // Set options third-party lib
         toastr.options.timeOut = 3000;
