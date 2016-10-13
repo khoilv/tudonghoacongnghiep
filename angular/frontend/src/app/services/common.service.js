41da9978e9c1e14da7db9fb51994cae8ec2363ca
khoilv <laivankhoi318@gmail.com>
2016-10-13 08:47:19 +0700

    Home page: Coding User Account Registration

--- angular/frontend/src/app/services/common.service.js
+++ angular/frontend/src/app/services/common.service.js
@@ -39,8 +39,44 @@
                 }
             };
 
+            var postData = function (url, queryParams, jsonp, onSuccess) {
+                var submitUrl = API_URL + url;
+                if (queryParams == null) queryParams = {};
+                if (jsonp) {
+                    angular.extend(queryParams, {callback: 'JSON_CALLBACK'});
+                    return $http({
+                        method: 'JSONP',
+                        url: submitUrl,
+                        params: queryParams,
+                        paramSerializer: '$httpParamSerializerJQLike'
+                    }).then(
+                        function (response) {
+                            if (typeof(onSuccess) === 'function') onSuccess(response.data);
+                        },
+                        function (response) {
+                            console.log(response);
+                        }
+                    );
+                } else {
+                    return $http({
+                        url: submitUrl,
+                        method: 'POST',
+                        params: queryParams,
+                        paramSerializer: '$httpParamSerializerJQLike'
+                    }).then(
+                        function (response) {
+                            if (typeof(onSuccess) === 'function') onSuccess(response.data);
+                        },
+                        function (response) {
+                            console.log(response);
+                        }
+                    );
+                }
+            };
+
             return {
-                loadData: loadData
+                loadData: loadData,
+                postData: postData
             }
         }]);
 
