f381e96a7c5d24a8aecf37143759e3c60a5ee583
khoilv <laivankhoi318@gmail.com>
2016-10-14 13:09:58 +0700

    Home page: Coding Customer Login

--- angular/frontend/src/app/services/login.service.js
+++ angular/frontend/src/app/services/login.service.js
@@ -3,7 +3,12 @@
     angular
         .module('angularSeedApp')
         .factory('loginService', ['$http', 'API_URL', function ($http, API_URL) {
+            var login = function (email, password) {
 
+            };
+            return {
+                login: login
+            }
         }]);
 
 })();
