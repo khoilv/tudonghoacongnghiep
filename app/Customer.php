48dbb0ca6ddd3e3e37db728614239f73283b347a
khoilv <laivankhoi318@gmail.com>
2016-10-16 09:58:59 +0700

    Home page: Coding Customer Login

--- app/Customer.php
+++ app/Customer.php
@@ -9,5 +9,20 @@ class Customer extends Authenticatable
 {
     protected $primaryKey = 'id';
     protected $table = 'customers';
-    protected $fillable = array('email', 'password', 'first_name', 'last_name', 'company', 'tel', 'address', 'city_province_id', 'created_at', 'updated_at');
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['email', 'password', 'first_name', 'last_name', 'company', 'tel', 'address', 'city_province_id', 'created_at', 'updated_at'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
