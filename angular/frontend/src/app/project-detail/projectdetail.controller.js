413dbc352aafbc722589d9b57f8bfba4f0724ca4
khoilv <laivankhoi318@gmail.com>
2016-10-19 23:59:17 +0700

    Project Detail

--- angular/frontend/src/app/project-detail/projectdetail.controller.js
+++ angular/frontend/src/app/project-detail/projectdetail.controller.js
@@ -13,27 +13,36 @@
         $scope.active = 0;
         $scope.noWrapSlides = false;
         $scope.slides = [];
+        $scope.mainItems = [];
+
+        var listProjectStatus = {
+            project_status_1: "Đã hoàn thành",
+            project_status_2: "Đang thực hiện",
+            project_status_3: "Chưa thực hiện"
+        };
 
         var url = 'project/' + $sanitize($stateParams.slug);
         commonService.loadData(url, {slug: $stateParams.slug}, function (response) {
-            $scope.project = response.data;
+            var project = response.data;
+            project.project_status = listProjectStatus['project_status_' + response.data.project_status];
+            $scope.project = project;
             //$scope.project.project_content = $sce.trustAsHtml(response.data.project_content);
-            initSlide(response.data.project_images);
+            initProjectImages(project.project_images);
         });
 
-
-
-        function initSlide(projectImages) {
-            var slide, items = [], itemCount = 0, slideCount = 0;
+        function initProjectImages(projectImages) {
+            var slide, slideItems = [], slideItemCount = 0, slideCount = 0;
             angular.forEach(projectImages, function (item, key) {
                 if (item.main == 0) {
-                    items.push(item);
-                    if (++itemCount % 3 == 0) {
+                    slideItems.push(item);
+                    if (++slideItemCount % 3 == 0) {
                         ++slideCount;
-                        slide = {id: slideCount - 1, items: items};
+                        slide = {id: slideCount - 1, items: slideItems};
                         $scope.slides.push(slide);
-                        items = [];
+                        slideItems = [];
                     }
+                } else {
+                    $scope.mainItems.push(item);
                 }
             });
             if (slideCount == 1 && slide) {
