72f047cdb3d3c8b899eb24c9c7babbadd8d49931
khoilv <laivankhoi318@gmail.com>
2016-10-20 11:03:02 +0700

    Project List => Pagination

--- angular/frontend/src/app/project-list/projectlist.controller.js
+++ angular/frontend/src/app/project-list/projectlist.controller.js
@@ -5,13 +5,25 @@
         .module('angularSeedApp')
         .controller('ProjectListController', ProjectListController);
 
-    ProjectListController.$inject = ['$scope', '$stateParams', 'commonService'];
+    ProjectListController.$inject = ['$scope', '$log', '$stateParams', 'commonService'];
 
     /** @ngInject */
-    function ProjectListController($scope, $stateParams, commonService) {
+    function ProjectListController($scope, $log, $stateParams, commonService) {
+        $scope.totalItems = 64;
+        $scope.currentPage = 1;
+        $scope.maxSize = 5;
+
         commonService.loadData('project/list', null, function (response) {
            $scope.projects = response.data;
         });
+
+        $scope.setPage = function (pageNo) {
+            $scope.currentPage = pageNo;
+        };
+
+        $scope.pageChanged = function() {
+            $log.log('Page changed to: ' + $scope.currentPage);
+        };
     }
 
 })();
