88fe88ee46908245dc7b062da1b9202cd563fb08
khoilv <laivankhoi318@gmail.com>
2016-10-21 23:16:53 +0700

    Address Book

--- angular/frontend/src/app/address-book/addressbook.controller.js
+++ angular/frontend/src/app/address-book/addressbook.controller.js
@@ -3,17 +3,44 @@
 
     angular
         .module('angularSeedApp')
-        .controller('AddressBookController', AddressBookController);
+        .controller('AddressBookController', AddressBookController)
+        .controller('NewAddressModalInstanceController', NewAddressModalInstanceController);
 
-
-    AddressBookController.$inject = ['$scope', 'commonService', 'customerService'];
+    AddressBookController.$inject = ['$scope', '$uibModal', 'commonService', 'customerService'];
+    NewAddressModalInstanceController.$inject = ['$scope', '$uibModalInstance', 'commonService'];
 
     /** @ngInject */
-    function AddressBookController($scope, commonService, customerService) {
+    function AddressBookController($scope, $uibModal, commonService, customerService) {
+
         var queryParams = {customer_id: customerService.getCustomerId()};
         commonService.loadData('customer/address-book', queryParams, function (response) {
             $scope.addresses = response.data;
         });
 
+
+        // add new address modal
+        $scope.openNewAddressModal = function () {
+            var modalInstance;
+
+            modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'newAddressModal.html',
+                size: 'lg',
+                controller: 'NewAddressModalInstanceController',
+                controllerAs: '$ctrl',
+                resolve: {}
+            });
+
+            modalInstance.result.then(function () {
+                $log.info('New address modal closed');
+            }, function () {
+                $log.info('New address modal dismissed at: ' + new Date());
+            });
+        };
     }
+
+    function NewAddressModalInstanceController($scope, $uibModalInstance, commonService) {
+
+    }
+
 })();
