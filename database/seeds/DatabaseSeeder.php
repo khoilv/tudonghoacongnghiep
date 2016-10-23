ed4d54671d8b37e174b85cd2cb8e161eaab07bc4
khoilv <laivankhoi318@gmail.com>
2016-10-24 00:16:49 +0700

    Add Brands table & data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MasterCitiesProvincesTableSeeder::class);
         $this->call(ProjectsTableSeeder::class);
         $this->call(AddressBookSeeder::class);
+        $this->call(BrandsTableSeeder::class);
     }
 }
