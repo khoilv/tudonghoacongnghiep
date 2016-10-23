8bbe07cd5e6b083d0a63ccbbd34a213eb5159d24
khoilv <laivankhoi318@gmail.com>
2016-10-23 11:01:18 +0700

    Account Info

--- angular/frontend/src/app/account-info/accountinfo.controller.js
+++ angular/frontend/src/app/account-info/accountinfo.controller.js
@@ -6,10 +6,22 @@
         .controller('AccountInfoController', AccountInfoController);
 
 
-    AccountInfoController.$inject = ['$scope', '$stateParams', 'commonService'];
+    AccountInfoController.$inject = ['$scope', 'commonService'];
 
     /** @ngInject */
-    function AccountInfoController($scope, $stateParams, commonService) {
+    function AccountInfoController($scope, commonService) {
+        $scope.account = {
+            city_province_id: ''
+        };
 
+        commonService.loadData('cities-provinces', null, function (response) {
+            var cities_provinces = [];
+            cities_provinces.push({id: '', name: '---Chọn Tỉnh/Thành phố---'});
+            angular.forEach(response.data, function (city_province) {
+                cities_provinces.push(city_province);
+            });
+            $scope.cities_provinces = cities_provinces;
+        });
     }
+
 })();
