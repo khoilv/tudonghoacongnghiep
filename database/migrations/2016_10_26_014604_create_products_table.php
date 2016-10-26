3fc798aebadc7628ff5b9c01b6ebbc69dd9131c0
khoilv <laivankhoi318@gmail.com>
2016-10-26 09:17:19 +0700

    Products

---
+++ database/migrations/2016_10_26_014604_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_category_id', false, true);
+            $table->integer('product_brand_id', false, true);
+            $table->string('product_code');
+            $table->string('product_title');
+            $table->integer('product_price', false, true);
+            $table->integer('product_price_discount', false, true);
+            $table->string('warranty_period');
+            $table->string('product_evaluation');
+            $table->text('product_description');
+            $table->text('product_images');
+            $table->text('product_keywords');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
