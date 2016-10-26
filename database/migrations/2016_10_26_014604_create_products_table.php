fbdd0d9d7db3221c3eff21da3e8ab43d6f806819
khoilv <laivankhoi318@gmail.com>
2016-10-26 13:26:40 +0700

    Products

--- database/migrations/2016_10_26_014604_create_products_table.php
+++ database/migrations/2016_10_26_014604_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             $table->integer('product_brand_id', false, true);
             $table->string('product_code');
             $table->string('product_title');
-            $table->string('product_url');
+            $table->string('product_url')->unique();
             $table->integer('product_price', false, true);
             $table->integer('product_price_discount', false, true);
             $table->float('discount_rate');
