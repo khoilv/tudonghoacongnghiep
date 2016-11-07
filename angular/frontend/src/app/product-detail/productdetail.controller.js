00d3a9a7d829f0694d2aa66420e6a039ad42c930
khoilv <laivankhoi318@gmail.com>
2016-11-07 16:25:27 +0700

    Product Detail

--- angular/frontend/src/app/product-detail/productdetail.controller.js
+++ angular/frontend/src/app/product-detail/productdetail.controller.js
@@ -11,11 +11,25 @@
     /** @ngInject */
     function ProductDetailController($scope, $stateParams, commonService) {
         var productUrl = $stateParams.product_url;
+        $scope.productImage = null;
 
         commonService.loadData('products/detail', {product_url: productUrl}, function (response) {
             console.log(response.data);
             $scope.product = response.data;
+            setProductImage(response.data.product_images);
         });
+
+        $scope.changeProductImage = function (productImage) {
+            $scope.productImage = productImage.image;
+        };
+
+        function setProductImage(productImages) {
+            angular.forEach(productImages, function (productImage) {
+               if (productImage.main == 1) {
+                   $scope.productImage = productImage.image;
+               }
+            });
+        }
     }
 
 })();
